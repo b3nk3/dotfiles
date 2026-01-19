d32d107c18e3fdcf3d383451d8d2778e766f63f5
Ben Szabo <me@benszabo.co.uk>
2026-01-19 08:40:24 +0000

    chore: update nixpkgs-stable reference to version 25.11 and enhance MacOS system settings in flake.nix

--- nix-darwin/flake.nix
+++ nix-darwin/flake.nix
@@ -5,7 +5,7 @@
   inputs = {
     # nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/*";
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    nixpkgs-stable.url = "github:NixOS/nixpkgs/nixos-25.05";
+    nixpkgs-stable.url = "github:NixOS/nixpkgs/nixos-25.11";
     nix-darwin.url = "github:LnL7/nix-darwin";
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
     nix-homebrew.url = "github:zhaofengli/nix-homebrew";
@@ -162,35 +162,80 @@
           # The platform the configuration will be used on.
           nixpkgs.hostPlatform = "aarch64-darwin";
 
-          # enable TouchID for sudo commands in terminal
-          security.pam.services.sudo_local.enable = true;
-          security.pam.services.sudo_local.touchIdAuth = true;
-          security.pam.services.sudo_local.text = "auth sufficient pam_tid.so.2"; # workaround...
+          security = {
+            # enable TouchID for sudo commands in terminal
+            pam.services.sudo_local.enable = true;
+            pam.services.sudo_local.touchIdAuth = true;
+            pam.services.sudo_local.text = "auth sufficient pam_tid.so.2"; # workaround...
+          };
 
           # sysytem settings
-          system.defaults.dock.orientation = "left";
-          system.defaults.dock.mineffect = "genie";
-          system.defaults.dock.minimize-to-application = true;
+          system.defaults = {
+            dock = {
+              orientation = "left";
+              mineffect = "genie";
+              minimize-to-application = true;
+              autohide = true;
+              show-recents = false;
+              showAppExposeGestureEnabled = true;
+              showDesktopGestureEnabled = true;
+              showMissionControlGestureEnabled = true;
+            };
+            finder = {
+              AppleShowAllFiles = true;
+              # When performing a search, search the current folder by default
+              FXDefaultSearchScope = "SCcf";
+              FXRemoveOldTrashItems = true;
+              ShowStatusBar = true;
+            };
+            trackpad = {
+              TrackpadThreeFingerDrag = true;
+              TrackpadFourFingerVertSwipeGesture = 2;
+            };
+            CustomUserPreferences = {
+              "com.apple.desktopservices" = {
+                # Avoid creating .DS_Store files on network or USB volumes
+                DSDontWriteNetworkStores = true;
+                DSDontWriteUSBStores = true;
+              };
+              "com.apple.finder" = {
 
-          system.defaults.dock.autohide = true;
-          system.defaults.dock.show-recents = false;
-          # needs logout/login to take effect
-          system.defaults.CustomUserPreferences = {
-            "com.apple.symbolichotkeys" = {
-              AppleSymbolicHotKeys = {
-                # Disable 'Cmd + Space' for Spotlight Search
-                # Key "64" is the identifier for the shortcut
-                "64" = {
-                  enabled = false;
+              };
+              "com.apple.symbolichotkeys" = {
+                AppleSymbolicHotKeys = {
+                  # Disable 'Cmd + Space' for Spotlight Search
+                  # Key "64" is the identifier for the shortcut
+                  "64" = {
+                    enabled = false;
+                  };
+                  # Optionally, disable 'Cmd + Alt + Space' for Finder search window
+                  # Key "65" is the identifier for the Finder search shortcut
+                  "65" = {
+                    enabled = false;
+                  };
+                  # Mission Control: Move left a space
+                  "79" = {
+                    enabled = true;
+                  };
+                  # Mission Control: Move right a space
+                  "81" = {
+                    enabled = true;
+                  };
+                  # Siri keyboard shortcut (Type to Siri)
+                  "176" = {
+                    enabled = false;
+                  };
                 };
-                # Optionally, disable 'Cmd + Alt + Space' for Finder search window
-                # Key "65" is the identifier for the Finder search shortcut
-                "65" = {
-                  enabled = false;
-                };
+              };
+              NSGlobalDomain = {
+                AppleShowAllFiles = true;
+                # Add a context menu item for showing the Web Inspector in web views
+                WebKitDeveloperExtras = true;
+                # "com.apple.trackpad.threeFingerDrag" = true;
               };
             };
           };
+          # needs logout/login to take effect
 
           # home manager
           users.users.benszabo.home = "/Users/benszabo";
